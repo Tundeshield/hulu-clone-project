06a95a74d6e357752d0eed34baca9cd8d37e93d1
Tunde Adepegba <tundeshield@gmail.com>
2021-01-19 20:51:08 +0100

    2. Design of the Nav component.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import Header from "./Header";
+import Nav from "./Nav";
 
 function App() {
   return (
     <div className="app">
     <Header/>
-    {/* Navigation */}
+    <Nav/>
     {/* Results */}
 
     </div>
