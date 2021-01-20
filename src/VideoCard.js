9865d110efac119179533714b3eb537f50f6bd14
Tunde Adepegba <tundeshield@gmail.com>
2021-01-20 04:40:42 +0100

    API fetching, videocard development and design.

---
+++ src/VideoCard.js
@@ -0,0 +1,28 @@
+import React,{forwardRef} from "react";
+import artemis from "./artemis.jpg";
+import "./VideoCard.css";
+import TextTruncate from "react-text-truncate";
+import ThumbUpSharpIcon from '@material-ui/icons/ThumbUpSharp';
+
+const base_url="https://image.tmdb.org/t/p/original/"
+const VideoCard=forwardRef(({movie}, ref)=>{
+    
+    return (
+        <div ref={ref} className="videoCard">
+            <img src={`${base_url}${movie.backdrop_path ||movie.poster_path}`} alt=""/>
+            <h2>{movie.title||movie.original_name}</h2>
+            <TextTruncate
+                line={1} element="p" truncateText="..."
+                text={movie.overview}
+            />
+             {/* <p>Release date: {movie.release_date || movie.first_air_date}</p> */}
+            
+           <p className="videoCard_stats">
+               {movie.media_type && `{movie.media_type} .`}
+               {movie.release_date|| movie.first_air_date}     <ThumbUpSharpIcon/>   {movie.vote_count}</p>
+            
+        </div>
+    )
+})
+
+export default VideoCard
